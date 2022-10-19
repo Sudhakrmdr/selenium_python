a440a6ae0a29fde97981a012245f677d039d5fbe
Sudhakrmdr <116163305+Sudhakrmdr@users.noreply.github.com>
2022-10-19 03:35:26 -0700

    Add files via upload

---
+++ StringExa.py
@@ -0,0 +1,55 @@
+# String Methods
+name = "Manjot"
+
+
+# print(len(name))
+# print(name.find('j'))
+# print(name.upper())
+# print(name.lower())
+# print(name.capitalize())
+# print(name.isdigit())
+# print(name.count("j"))
+# print(name.replace("a", "X"))
+# print(name * 10)
+sli = "hello"
+# print(sli[0:5])
+# print(sli[6:10])
+# print(sli)
+# print(sli[:5])
+# print(sli[6:])
+# print(sli[-1])
+# print(sli[-6])
+# print(sli[::-1])
+
+# spl = "www.ibm.com"
+# print(spl.split("."))
+s = "        Hello "
+# print("Before Strip", len(s))
+
+# sstr = s.strip()
+# print("After Strip", len(sstr))
+
+# lsstr = s.lstrip()
+# print(len("Only removes left hand side spaces", lsstr))
+
+# rsstr = s.rstrip()
+# print(len("Only removes right hand side spaces", rsstr))
+
+# concatenate:
+a = "Manjot"
+b = "How are you"
+
+print(a + " " + b)
+
+# Type casting
+# converting one data type to another datatype
+# x = 10.90
+# print(int(x))
+# print(type(x))
+
+# y = "3"
+# print(y * 7)
+# print(int(y) * 8)
+
+
+
---
+++ dataTypes.py
@@ -0,0 +1,29 @@
+# print("Man world")
+# name = "manjot"
+# Name = 'Khan'
+
+# price = 100 +10
+# boo = True
+# height = 5.6
+
+# print("name of the student ", name)
+# print(price)
+
+# price = "8545348%%"
+# price_one = 454.56
+# att = False
+# print(type(price))
+# print(price)
+# print(type(att))
+#
+# name, age, attractive = "Manjot", 26, True
+# print(name)
+
+Manjot = Khan = Chitra = "Automation Test Engineers"
+print("Designation of Manjot", Manjot)
+print("Designation of Khan", Khan)
+print("Designation of Chitra", Chitra)
+#
+# print(len())
+
+
